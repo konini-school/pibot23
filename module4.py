b7605576ce7d29454b7891ce58c76e0a6528b722
dev-git <dev-git@users.noreply.github.com>
2017-11-08 11:34:36 +1300

    Update module4.py

--- module4.py
+++ module4.py
@@ -24,18 +24,17 @@ try:
 
     print 'Turn on red light\n'
     GPIO.output(11, True)
-    time.sleep(7)
+    time.sleep(2)
     GPIO.output(11, False)
     
     print 'Turn on orange light\n'    
     GPIO.output(13, True)
-    time.sleep(6)
+    time.sleep(2)
     GPIO.output(13, False)
     
     print 'Turn on green light\n'    
     GPIO.output(15, True)
-    time.sleep(4)
-             
+    time.sleep(2)
     GPIO.output(15, False)
 
 # If CTRL+C is pressed the main loop is broken
